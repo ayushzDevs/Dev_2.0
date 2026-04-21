f64401c5f2fe746609d4edcc339530dc72c4272e
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-21 19:57:46 +0530

    21 april

--- express_dir/index.js
+++ express_dir/index.js
@@ -11,10 +11,24 @@ app.listen(port,()=>{
     console.log(`app is listening on ${port}`);
 });
 
-app.use((req , res)=>{
-    // console.log(req);
-    console.log("request recieved");
-    res.send(
-        "<h1>Hello, World!</h1>"
-    );
-});
+// app.use((req , res)=>{
+//     // console.log(req);
+//     console.log("request recieved");
+//     res.send(
+//         "<h1>Hello, World!</h1>"
+//     );
+// });
+
+
+
+app.get("/",(req,res)=>{
+    res.send("you contacted root path");
+})
+
+app.get("/search",(req,res)=>{
+    res.send("you contacted search path");
+})
+
+app.get("/help",(req,res)=>{
+    res.send("you contacted help path");
+})
