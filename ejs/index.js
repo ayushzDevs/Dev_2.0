f1c2ed43ae238124a6d17b4662e1bd4b90d2fa25
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-23 10:12:33 +0530

    23 april

--- ejs/index.js
+++ ejs/index.js
@@ -8,11 +8,19 @@ const path = require("path");
 app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"/views"));
 
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
+
 app.get("/hello",(req,res)=>{
     res.render("home.ejs");
 
 });
 
+app.get("/rolldice",(req,res)=>{
+    res.render("rolldice.ejs");
+    
+});
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
