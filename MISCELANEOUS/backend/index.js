534c6433114451b12e7d6046fb8921308c75c6f0
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-23 20:08:11 +0530

    23 april

---
+++ MISCELANEOUS/backend/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.listen(port,()=>{
+    console.log(`app is listening on ${port}`);
+});
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello i am root");
+});
+
+app.get("/register",(req,res)=>{
+    let {user , password} = req.query;
+    res.send(`hello ${user}`);
+});
+
+app.post("/register",(req,res)=>{
+    
+    let {user , password} = req.body;
+    res.send(`hello ${user}`);
+});
+
+
+
+
+
+
