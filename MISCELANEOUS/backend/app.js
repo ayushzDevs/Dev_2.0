7467e4f036a74c8aba3f24a9b3cd24a4603e885c
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-25 19:09:46 +0530

    25 april

--- MISCELANEOUS/backend/app.js
+++ MISCELANEOUS/backend/app.js
@@ -1,15 +1,44 @@
-function personmaker(name,age){
-    const person = {
-        name:name,
-        age:age,
-        talk(){
-            console.log(`My name is ${this.name} and I am ${this.age} years old.`);
-        },
-    };
-    return person;
-}
+// factory function
 
-let p1 = personmaker("ayush",21);
-p1.talk();
-let p2 = personmaker("shohini",20);
-p2.talk();
+// function personmaker(name,age){
+//     const person = {
+//         name:name,
+//         age:age,
+//         talk(){
+//             console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+//         },
+//     };
+//     return person;
+// }
+
+// let p1 = personmaker("ayush",21);
+// p1.talk();
+// let p2 = personmaker("shohini",20);
+// p2.talk();
+
+
+
+
+
+
+
+
+
+// constructor function -> it doesn't return anything, it is used with the 'new' keyword
+
+function Person(name,age){
+    const person = {
+        name : name,
+        age : age,
+    }
+};
+
+
+let p1 = new Person("Ayush",21);
+let p2 = new Person("Shohini",20);
+
+Person.prototype.talk = function(){
+            console.log(`my name is ${this.name} and my age is ${this.age}`);
+        
+};
+
