09a2a3d3c80a1d0a065ea191f9dcc6c2da705233
ayushzAIML <theayushchakraborty@gmail.com>
2026-04-26 18:47:34 +0530

    26 april

--- Project_using_REST/index.js
+++ Project_using_REST/index.js
@@ -7,12 +7,28 @@ const path = require("path");
 // middlewares
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Server working well");
+
+let posts = [
+    {
+        username : "ayush",
+        content : "This is my first post"
+    },
+    {
+        username : "shohini",
+        content : "This is my second post"
+    },
+    {
+        username : "baban",
+        content : "This is my third post"
+    },
+];
+
+app.get("/posts",(req,res)=>{
+    res.render("index.ejs",{posts:posts});
 })
 
 app.listen(port,()=>{
