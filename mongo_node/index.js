fee9101d0f293a6cdbc20d29a9a9de4a003d931f
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-07 19:55:43 +0530

    7 may

--- mongo_node/index.js
+++ mongo_node/index.js
@@ -11,3 +11,13 @@ main().then((res)=>{ console.log('Connected to MongoDB');})
 async function main() {
   await mongoose.connect(process.env.MONGODB_URI);
 }
+
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  age : Number
+})
+
+const User = mongoose.model("User", userSchema);
+const Emloyee = mongoose.model("Employee", userSchema);
