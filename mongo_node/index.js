0c471e1e6738ea971b9c4a4fe5c87cc12784f6bf
ayushzAIML <theayushchakraborty@gmail.com>
2026-05-08 11:51:58 +0530

    8 may

--- mongo_node/index.js
+++ mongo_node/index.js
@@ -26,10 +26,14 @@ const emp1 = new Employee({
 // .then(() => console.log('Employee saved'))
 // .catch(err => console.log(err));
 
-Employee.insertMany([
-  { name: 'Alice Smith', age: 28, email: 'ashjj@mail.com'},
-  { name: 'Bob Johnson', age: 35, email: 'mmajbdj@mail.com'},
-  { name: 'Charlie Brown', age: 22, email: 'mmmmmm@mmmmmail.com'}
-])
-.then(()=> console.log('Employees inserted'))
-.catch(err => console.log(err));
+// Employee.insertMany([
+//   { name: 'Alice Smith', age: 28, email: 'ashjj@mail.com'},
+//   { name: 'Bob Johnson', age: 35, email: 'mmajbdj@mail.com'},
+//   { name: 'Charlie Brown', age: 22, email: 'mmmmmm@mmmmmail.com'}
+// ])
+// .then(()=> console.log('Employees inserted'))
+// .catch(err => console.log(err));
+
+Employee.find({age:{$gt:25}}).then((res)=>{
+  console.log(res);
+}).catch(err => console.log(err));
